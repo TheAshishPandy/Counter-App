9c481364e5f4019875997e0f8328dcc0bcab6e40
Ashish Pandey <Ashish.pandey@businessnext.com>
2025-08-15 20:49:47 +0530

    Create the counter Application in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Counter-app');
+  value=0;
+  handleIncrement(){
+    this.value=this.value+1;
+  }
+  handleDecrement(){
+    this.value=this.value-1;
+  }
+
+  handleCounter(v:number){
+    if(v==1){
+      this.value=this.value+1;
+    }
+    else if(v==2){
+      
+        this.value=this.value<=0?0:this.value-1;
+    }
+    else{
+      this.value=0;
+    }
+
+
+  }
 }
